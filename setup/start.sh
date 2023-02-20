aaed128704f6a49a859d0402eca8a491c9719290
Blake Johnston <johnstonbl01@gmail.com>
2023-02-19 18:27:43 -0600

    complete ansible setup migration

--- setup/start.sh
+++ setup/start.sh
@@ -4,16 +4,10 @@
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Set homebrew path
-echo -e "\n# Set PATH for Homebrew" >> $HOME/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bashrc
-source $HOME/.bashrc
+echo -e "\n# Set PATH for Homebrew" >> ~/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+source ~/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew install ansible
 ansible-playbook setup.yml
-
-# Switch to zsh
-zsh
-
-echo "**** Don't forget to source bash profile ****"
-
